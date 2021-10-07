f89f20c081b0752dfe1aba02e4d8e16ccd8928ea
KenrickOliver <kenrick.oliver@gmail.com>
2021-10-07 14:54:22 -0400

    added simple responsiveness to nav

--- P5-StickyNavbar/script.js
+++ P5-StickyNavbar/script.js
@@ -1,12 +1,26 @@
-const nav = document.querySelector('.nav')
-window.addEventListener('scroll', fixedNav)
+const nav = document.querySelector(".navbar");
+const logo = document.querySelector(".logo");
+window.addEventListener("scroll", fixedNav);
 
+//sticly nav
 function fixedNav() {
-    // console.log(window.scrollY, nav.offsetHeight)
-    if(window.scrollY > nav.offsetHeight + 150) {
-        nav.classList.add('nav-active')
-    } else {
-        nav.classList.remove('nav-active')
-    }
+  // console.log(window.scrollY, nav.offsetHeight)
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("nav-active");
+    logo.classList.add("logo-active");
+  } else {
+    nav.classList.remove("nav-active");
+    logo.classList.remove("logo-active");
+  }
 }
 
+//responsive nav
+const toggle = document.querySelector(".toggle__button");
+const navLinks = document.querySelector(".navbar__links");
+const toggleIcon = document.querySelector(".toggle__icon");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  toggleIcon.classList.toggle("bx-menu");
+  toggleIcon.classList.toggle("bx-x");
+});
